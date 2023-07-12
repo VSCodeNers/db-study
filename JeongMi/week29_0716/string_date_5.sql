4604ffe488c3830eea3990ea48b573ac5fa20dc8
jung0115 <jungim7490@gmail.com>
2023-07-12 19:59:47 +0900

    29주차 완

---
+++ JeongMi/week29_0716/string_date_5.sql
@@ -0,0 +1,5 @@
+-- String, Date: 루시와 엘라 찾기(Lv.2)
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID;
