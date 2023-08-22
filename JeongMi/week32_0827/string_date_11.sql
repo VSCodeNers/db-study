300a72021829be37fcafcc8698341ee140e61b0a
jung0115 <jungim7490@gmail.com>
2023-08-22 22:11:08 +0900

    [정미] 32주차 완

---
+++ JeongMi/week32_0827/string_date_11.sql
@@ -0,0 +1,8 @@
+-- String, Date: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기(Lv.3)
+SELECT CONCAT('/home/grep/src/', BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) as FILE_PATH
+FROM USED_GOODS_FILE 
+WHERE BOARD_ID LIKE (SELECT BOARD_ID
+      FROM USED_GOODS_BOARD
+     ORDER BY VIEWS DESC
+     LIMIT 1)
+ ORDER BY FILE_ID DESC;
